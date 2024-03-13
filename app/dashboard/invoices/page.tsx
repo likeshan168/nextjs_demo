8295ef8504c42e3dda9b6b8637653a243a780526
likeshan <likeshan@gwjia.com.cn>
2024-03-13 23:05:17 +0800

    add chapter1-5 examples code

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <div>
+            <p>Invoices Page</p>
+            <div>
+                <button className="bg-red-500 btn">hello world</button>
+            </div>
+        </div>
+    );
+}
